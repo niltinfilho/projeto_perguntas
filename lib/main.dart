018db2ac518912f142312998fdee0cc04f6495c4
Nilton Filho <niltonfilho115@gmail.com>
2024-07-09 19:54:38 -0300

    criando o titulo e iniciando o corpo do projeto

--- lib/main.dart
+++ lib/main.dart
@@ -1,17 +1,26 @@
 import 'package:flutter/material.dart';
 
-main() {
-  runApp(new PerguntaApp());
-}
+// main() {
+//   runApp(new PerguntaApp());
+// }
+
+// esse eh o topo da arvore de componentes no flutter, todos os widgets estao abaixo do "PerguntaApp"
+main() => runApp(PerguntaApp());
 
 // a classe "PerguntaApp" herda a classe "StatelessWidget", ou seja, possui essas caracteristicas
 class PerguntaApp extends StatelessWidget {
+
+  @override
   // a funcao "build" recebe um parametro e retorna um widget
   Widget build(BuildContext context) {
     // esta retornando uma nova instancia da classe "MaterialApp"
     return MaterialApp(
-      // a classe "MaterialApp" recebe varios parametros, dentre eles o "Text"
-      home: Text('Ola, Flutter!'),
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('Perguntas'),
+        ),
+        body: Text('Ola, Flutter!'),
+      ),
     );
   }
 }
