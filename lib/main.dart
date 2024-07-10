69adcdf46998a524749406bc722ce5d899218790
Nilton Filho <niltonfilho115@gmail.com>
2024-07-09 21:18:25 -0300

    criando a estrutura e os botoes de respostas

--- lib/main.dart
+++ lib/main.dart
@@ -10,16 +10,41 @@ main() => runApp(PerguntaApp());
 // a classe "PerguntaApp" herda a classe "StatelessWidget", ou seja, possui essas caracteristicas
 class PerguntaApp extends StatelessWidget {
 
+  void responder() {
+    print('Pergunta respondida!');
+  }
+
   @override
   // a funcao "build" recebe um parametro e retorna um widget
   Widget build(BuildContext context) {
+    final perguntas = [
+      'Qual eh a sua cor favorita?',
+      'Qual eh o seu animal favorito?',
+    ];
+
     // esta retornando uma nova instancia da classe "MaterialApp"
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
           title: Text('Perguntas'),
         ),
-        body: Text('Ola, Flutter!'),
+        body: Column(
+          children: [
+            Text(perguntas[0]),
+            ElevatedButton(
+              onPressed: responder,
+              child: Text('Resposta 1'),
+            ),
+            ElevatedButton(
+              onPressed: responder,
+              child: Text('Resposta 2'),
+            ),
+            ElevatedButton(
+              onPressed: responder,
+              child: Text('Resposta 3'),
+            ),
+          ],
+        ),
       ),
     );
   }
