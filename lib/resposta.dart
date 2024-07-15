461b409d07f9086207b9032a858fbe9d8c25116f
Nilton Filho <niltonfilho115@gmail.com>
2024-07-15 00:38:12 -0300

    estilizando os botoes de resposta

--- lib/resposta.dart
+++ lib/resposta.dart
@@ -3,14 +3,22 @@ import 'package:flutter/material.dart';
 class Resposta extends StatelessWidget {
 
   String texto = '';
+  final void Function() quandoSelecionado;
 
-  Resposta(this.texto);
+  Resposta(this.texto, this.quandoSelecionado);
 
   @override
   Widget build(BuildContext context) {
-    return ElevatedButton(
-      onPressed: null,
-      child: Text(texto),
+    return Container(
+      width: double.infinity,
+      child: ElevatedButton(
+        style: ElevatedButton.styleFrom(
+          foregroundColor: Colors.blue,
+          backgroundColor: Colors.black, // Background color
+        ),
+        onPressed: quandoSelecionado,
+        child: Text(texto),
+      ),
     );
   }
 }
