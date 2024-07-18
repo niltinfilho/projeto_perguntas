1a76793f2b3789e05a0c8d25dda9872f73dc1379
Nilton Filho <niltonfilho115@gmail.com>
2024-07-17 23:01:24 -0300

    corrigindo erro quando acaba as perguntas

--- lib/main.dart
+++ lib/main.dart
@@ -11,16 +11,7 @@ main() => runApp(PerguntaApp());
 class _PerguntaAppState extends State<PerguntaApp> {
   var _perguntaSelecionada = 0;
 
-  void _responder() {
-    setState(() {
-      _perguntaSelecionada++;
-    });
-  }
-
-  @override
-  // a funcao "build" recebe um parametro e retorna um widget
-  Widget build(BuildContext context) {
-    final List<Map<String, Object>> perguntas = [
+  final List<Map<String, Object>> _perguntas = const [
       {
         'texto': 'Qual eh a sua cor favorita?',
         'respostas': ['Preto', 'Vermelho', 'Verde', 'Branco'],
@@ -35,8 +26,25 @@ class _PerguntaAppState extends State<PerguntaApp> {
       }
     ];
 
-    List<String> respostas = perguntas[_perguntaSelecionada]
-      .cast()['respostas'];
+  void _responder() {
+    if(temPerguntaSelecionada) {
+      setState(() {
+        _perguntaSelecionada++;
+      });
+    }
+  }
+
+  bool get temPerguntaSelecionada {
+    return _perguntaSelecionada < _perguntas.length;
+  }
+
+  @override
+  // a funcao "build" recebe um parametro e retorna um widget
+  Widget build(BuildContext context) {
+    
+    List<String> respostas = temPerguntaSelecionada
+      ? _perguntas[_perguntaSelecionada].cast()['respostas']
+      : [];
 
     // esta retornando uma nova instancia da classe "MaterialApp"
     return MaterialApp(
@@ -44,12 +52,12 @@ class _PerguntaAppState extends State<PerguntaApp> {
         appBar: AppBar(
           title: Text('Perguntas'),
         ),
-        body: Column(
+        body: temPerguntaSelecionada ? Column(
           children: [
-            Questao(perguntas[_perguntaSelecionada]['texto'].toString()),
+            Questao(_perguntas[_perguntaSelecionada]['texto'].toString()),
             ...respostas.map((t) => Resposta(t, _responder)),
           ],
-        ),
+        ) : null,
       ),
     );
   }
